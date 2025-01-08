5b176a42e5a09de4a6f00c070a25356a8a562f23
Shubham Tiwari <shubht3303@gmail.com>
2025-01-09 05:08:41 +0530

    wallet connect

--- src/components/spinner.jsx
+++
@@ -1,6 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export function Spinner(props) {
-  const size = props.size || 28;
-  return <Loader2 height={size} width={size} className={`animate-spin`} />;
-}
